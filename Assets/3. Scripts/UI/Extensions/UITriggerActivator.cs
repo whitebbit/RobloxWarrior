58aad4ab7d877f41da76e8d37d2156e08c9f1cca
Yan Bratushchik <whitebbit.entertainment@gmail.com>
2024-12-16 18:33:48 +0300

    udpate world

---
+++ Assets/3. Scripts/UI/Extensions/UITriggerActivator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using _3._Scripts.UI.Panels;
+using UnityEngine;
+
+namespace _3._Scripts.UI.Extensions
+{
+    public class UITriggerActivator : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out Player.Player _))
+            {
+                UIManager.Instance.GetPanel<HeroPanel>().Enabled = true;
+            }
+        }
+    }
+}
